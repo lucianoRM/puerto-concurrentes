cf81ec4950179a36325fc0b205241593e00a3c02
lucianoRM <luciano.rmna@gmail.com>
2015-10-22 15:41:40 -0300

    Modifico log de grua para que diga bien a donde pone las cargas

--- src/Grua.cpp
+++ src/Grua.cpp
@@ -12,21 +12,26 @@ void Grua::run(Controlador* controlador){
 
     struct trabajo trabajo;
     std::string transporte;
+    std::string destino;
     Logger::getInstance()->log("[GRUA] Voy a empezar mi trabajo");
 
     trabajo = controlador->asignarTrabajoAGrua();
 
     if (trabajo.tipoTrabajo == DESCARGAR_CAMION) {
         transporte = "CAMION";
+        destino = "BARCO";
     } else {
         transporte = "BARCO";
+        destino = "CAMION";
     }
 
     Logger::getInstance()->log("[GRUA] Tome la carga del " + transporte + ": " + std::to_string(trabajo.proceso) + ", tenia carga de: " + std::to_string(trabajo.carga));
 
     pid_t pidTransporte = controlador->tomarTransporteVacio(1 - trabajo.tipoTrabajo);
 
-    Logger::getInstance()->log("[GRUA] Tengo asignado a " + transporte + ": " + std::to_string(pidTransporte) + " para cargarlo");
+
+
+    Logger::getInstance()->log("[GRUA] Tengo asignado a " + destino + ": " + std::to_string(pidTransporte) + " para cargarlo");
 
     controlador->descargarGrua(trabajo,pidTransporte);
 }
